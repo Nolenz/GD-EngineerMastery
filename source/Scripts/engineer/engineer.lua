e3c20c9ac663dd930e02798e8fd946f0c525b89f
Nick Olenz <nickolenz@comcast.net>
2016-06-25 00:47:11 -0500

    Teleporting pets attack now, teleport controller.

--- source/Scripts/engineer/engineer.lua
+++ source/Scripts/engineer/engineer.lua
@@ -4,20 +4,21 @@ function gd.engineer.testOnAddToWorld(objectId)
 	UI.Notify("Still Running")
 	entityId = objectId
 	entity = Entity.Get(objectId)
-	--spawn = Character.Create("records/skills/playerclassengineer/pets/pet_sentry_01.dbr")
+	--spawnOne = Character.Create("records/skills/playerclassengineer/pets/flamer_script_test.dbr")
 
-	count = 0
 	Script.RegisterForUpdate(entityId, "Update", 10)
-	--UI.Notify("Update Registered")
 end
 
+--function gd.engineer.testOnDelete()
+--	spawnOne:Destroy()
+--end
 
 function Update()
-	count = count+1
 	coords = Game.GetLocalPlayer():GetCoords()
 	entity:SetCoords(coords)
-	if (count > 1000) then
-		Script.UnregisterForUpdate(entityId, "Update")
-		entity:Destroy()
-	end
+--	spawnOne:SetCoords(coords)
+--	if (count > 1000) then
+--		Script.UnregisterForUpdate(entityId, "Update")
+--		entity:Destroy()
+--	end
 end
