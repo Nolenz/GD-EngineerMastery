9f38874235c938ec470a2f3da2b84c1b1e33063a
Nick Olenz <nickolenz@comcast.net>
2016-06-24 23:39:09 -0500

    Moving pets does not move targets. Ugh.

--- source/Scripts/engineer/engineer.lua
+++ source/Scripts/engineer/engineer.lua
@@ -1,5 +1,23 @@
 gd.engineer = {}
 
 function gd.engineer.testOnAddToWorld(objectId)
-	UI.Notify("Hello World! Please?")
+	UI.Notify("Still Running")
+	entityId = objectId
+	entity = Entity.Get(objectId)
+	--spawn = Character.Create("records/skills/playerclassengineer/pets/pet_sentry_01.dbr")
+
+	count = 0
+	Script.RegisterForUpdate(entityId, "Update", 10)
+	--UI.Notify("Update Registered")
+end
+
+
+function Update()
+	count = count+1
+	coords = Game.GetLocalPlayer():GetCoords()
+	entity:SetCoords(coords)
+	if (count > 1000) then
+		Script.UnregisterForUpdate(entityId, "Update")
+		entity:Destroy()
+	end
 end
